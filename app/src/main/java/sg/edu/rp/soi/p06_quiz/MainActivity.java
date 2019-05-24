0191f8974ef20860d56ab9e665ce30699761124a
18033405 <18033405@myrp.edu.sg>
2019-05-24 15:33:34 +0800

    i am see toh wei keong

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+//check in by 18033405
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
